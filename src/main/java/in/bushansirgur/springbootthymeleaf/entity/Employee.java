6bf6b1053f5b7266700b832ea505d5def544fb11
Ricardo <58637839+ritchieramosz@users.noreply.github.com>
2024-04-15 17:38:42 -0600

    Guardando el repositorio

---
+++ src/main/java/in/bushansirgur/springbootthymeleaf/entity/Employee.java
@@ -0,0 +1,31 @@
+package in.bushansirgur.springbootthymeleaf.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tbl_votantes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+	
+	private String clave;
+	
+	private String seccion;
+
+	private int partido;
+}
